9fa55346f87ea8b01b643d2c6376ad0a899da7b2
Midnight Doggo <git@midnightdoggo19.com>
2025-02-02 20:37:00 +0000

    tests

--- __tests__/package.test.js
+++ __tests__/package.test.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-<<<<<<< HEAD
-=======
-const validateNpmPackage = require('validate-npm-package-name');
->>>>>>> 6890512a7873675156d5955965b682b056e12ae8
 
 const packageJsonPath = path.resolve(__dirname, '../package.json');
 let packageJson;
@@ -14,22 +10,7 @@ beforeAll(() => {
     packageJson = JSON.parse(fileContent);
 });
 
-<<<<<<< HEAD
 describe('dependencies tests', () => {
-=======
-describe('package.json tests', () => {
-    test('should have a valid structure', () => {
-        expect(packageJson).toHaveProperty('main');
-        expect(packageJson).toHaveProperty('scripts');
-        expect(packageJson).toHaveProperty('dependencies');
-        expect(packageJson).toHaveProperty('devDependencies');
-    });
-
-    test('should have a start script', () => {
-        expect(packageJson.scripts).toHaveProperty('start');
-    });
-
->>>>>>> 6890512a7873675156d5955965b682b056e12ae8
     test('dependencies should not include duplicates in devDependencies', () => {
         const deps = Object.keys(packageJson.dependencies || {});
         const devDeps = Object.keys(packageJson.devDependencies || {});
